8a99c4989d32c0c7adbeb4467b5fef6baeeb8121
Cenxi Si <cenxi@student.unimelb.edu.au>
2022-05-08 16:01:28 +0800

    add ansible

---
+++ Ansible/run-instance.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Cluster and Cloud Computing - Team 20
+# 
+# Authors: 
+#
+#  Cenxi Si, Student ID: 1052447
+#  Yipei Liu, Student ID: 1067990
+#  Jingdan Zhang, Student ID: 1054101
+#  Chengyan Dai, Student ID: 1054219
+#  Ruimin Sun, Student ID: 1052182
+#
+
+. ./openrc.sh; ansible-playbook instance.yaml
