b73a14b74646c04ecdf911527f5eb4f0d08313e0
chengyan911 <88361785+chengyan911@users.noreply.github.com>
2022-05-12 01:48:35 +1000

    Update melResultRouter.js

--- Web/melResultRouter.js
+++ Web/melResultRouter.js
@@ -1,3 +1,18 @@
+//
+// Part of COMP90024 Cluster and Cloud Computing from The University of Melbourne 
+//
+// Assignment 2 - Team 20
+//
+// Collective Team Details (Member's Name/Student ID/Location): 
+//
+//  * Cenxi Si 1052447 China
+//  * Yipei Liu 1067990 China
+//  * Jingdan Zhang 1054101 China
+//  * Chengyan Dai 1054219 China
+//  * Ruimin Sun 1052182 China
+//
+
+
 const express = require('express')
 
 // add our router
